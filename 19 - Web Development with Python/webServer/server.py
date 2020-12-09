f04d839e714b4db55ce53e03b7c37c546b2ec0cf
ThiagoBuen <thiagobueno@gea.inatel.br>
2020-12-08 21:35:43 -0300

    08/12 - Building a web server

--- 19 - Web Development with Python/webServer/server.py
+++ 19 - Web Development with Python/webServer/server.py
@@ -2,18 +2,29 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('./index.html')
+def my_home():
+    return render_template('index.html')
 
-@app.route('/about')
+@app.route('/index.html')
+def home():
+    return render_template('index.html')
+
+@app.route('/about.html')
 def about():
-    return render_template('./about.html')
+    return render_template('about.html')
 
-@app.route('/blog')
-def blog():
-    return 'Some gibberish!!!'
+@app.route('/work.html')
+def work():
+    return render_template('work.html')
     
-@app.route('/blog/2020/dogs')
-def blog2():
-    return 'Some gibberish about dog!!!'
+@app.route('/works.html')
+def works():
+    return render_template('works.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
     
+@app.route('/components.html')
+def components():
+    return render_template('components.html')
