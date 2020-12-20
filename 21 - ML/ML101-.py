944b1d0f76960bb0d98b5c06c214ab6c0e2d5fdb
ThiagoBuen <thiagobueno@gea.inatel.br>
2020-12-20 17:04:26 -0300

    20/12 - Data Science baby

---
+++ 21 - ML/ML101-.py
@@ -0,0 +1,19 @@
+# 1  -  Import the data
+
+# 2 - Clean the data
+
+# 3 - Split the data -> Training Set/Test Set
+
+# 4 - Create a Model
+
+# 5 - Check the output
+
+# 6 - Improve
+
+
+# Python Tools
+
+### Numpy        --- List, arrrays
+# Pandas       --- Data analysism data manipulation
+# Scikit-learn --- Create a model, different types of algorithms
+# Matplotlib   ---- Charting library, present the graphs
